91f878db47f34dd51023f407339ffefd477edf58
jaso Sorenson <jaso@cs.wisc.edu>
2023-05-05 19:18:02 -0500

    Set up initial logging for the gossiper.

--- src/cs739/gossiper/Gossiper.java
+++ src/cs739/gossiper/Gossiper.java
@@ -1,9 +1,22 @@
 package cs739.gossiper;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
+
 public class Gossiper {
+	 private static final Logger logger = LogManager.getLogger(Gossiper.class);
+
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		
+		   Configurator.initialize(null, "log4j2.xml");
+
+		    // Use the logger to log messages
+		    logger.debug("Debug message");
+		    logger.info("Info message");
+
 
 	}
 
