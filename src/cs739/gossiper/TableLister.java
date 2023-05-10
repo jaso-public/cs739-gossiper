8ffa68c6355e2ad5d757554996d5e8e26f362f17
spencrr <23708360+spencrr@users.noreply.github.com>
2023-05-09 22:58:01 -0500

    Update TableLister.java

--- src/cs739/gossiper/TableLister.java
+++ src/cs739/gossiper/TableLister.java
@@ -1,7 +1,10 @@
 package cs739.gossiper;
 
-import java.io.*;
-import java.util.*;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
@@ -11,50 +14,48 @@ import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.dynamodbv2.model.ScanRequest;
 import com.amazonaws.services.dynamodbv2.model.ScanResult;
 
-
 /**
  * Dumps the contents of the DynamoDB table containing the gossiper events.
  * (Put your credentials and region in ~/.aws/credentials)
  */
 public class TableLister {
-	
-    static Map<String, List<String>> table = new HashMap<String,List<String>>() {};
 
-    public static void add_item(String key, String value){
-        if(table.get(key) == null){
+    static Map<String, List<String>> table = new HashMap<String, List<String>>() {
+    };
+
+    public static void add_item(String key, String value) {
+        if (table.get(key) == null) {
             table.put(key, new ArrayList<String>());
         }
         table.get(key).add(value);
     }
 
-    public static void even_table(){
+    public static void even_table() {
         int max = -1;
 
         for (var list : table.entrySet()) {
-            if(max < list.getValue().size()){
+            if (max < list.getValue().size()) {
                 max = list.getValue().size();
             }
         }
 
         for (var list : table.entrySet()) {
-            while(max > list.getValue().size()){
-                list.getValue().add(null);
+            while (max > list.getValue().size()) {
+                list.getValue().add("");
             }
         }
     }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		// Create AWS credentials using the access key and secret key
-		AWSCredentialsProvider credentialsProvider = new ProfileCredentialsProvider(Constants.profile);
-        
+        // Create AWS credentials using the access key and secret key
+        AWSCredentialsProvider credentialsProvider = new ProfileCredentialsProvider(Constants.profile);
+
         // Create a DynamoDB client using the credentials
         AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
-            .withCredentials(credentialsProvider)
-            .withRegion(Constants.region)
-            .build();
-        
-        
+                .withCredentials(credentialsProvider)
+                .withRegion(Constants.region)
+                .build();
 
         ScanRequest scanRequest = new ScanRequest().withTableName(Constants.tableName);
         ScanResult result = null;
@@ -66,21 +67,24 @@ public class TableLister {
             List<Map<String, AttributeValue>> items = result.getItems();
             for (Map<String, AttributeValue> item : items) {
 
-            	String application = item.get(Constants.partitionKey).getS();
+                String application = item.get(Constants.partitionKey).getS();
                 String timestamp = item.get(Constants.sortKey).getN();
 
                 add_item("application", application);
                 add_item("timestamp", timestamp);
+                add_item("timestamp-date", new Timestamp((long) Double.parseDouble(timestamp)).toString());
 
-                for(Map.Entry<String, AttributeValue> value : item.entrySet()) {
-                	String name = value.getKey();
-                  	if(Constants.partitionKey.equals(name)) continue;
-                  	if(Constants.sortKey.equals(name)) continue;
-                           	
+                for (Map.Entry<String, AttributeValue> value : item.entrySet()) {
+                    String name = value.getKey();
+                    if (Constants.partitionKey.equals(name))
+                        continue;
+                    if (Constants.sortKey.equals(name))
+                        continue;
+
                     add_item(name, value.getValue().getS());
                 }
                 even_table();
-             }
+            }
         } while (result.getLastEvaluatedKey() != null);
 
         List<String> keys = new ArrayList<String>();
@@ -92,12 +96,12 @@ public class TableLister {
         }
         System.out.println("");
 
-        for(int i = 0; i < num_rows; i++){
+        for (int i = 0; i < num_rows; i++) {
             for (String string : keys) {
                 System.out.print(table.get(string).get(i) + ",");
             }
             System.out.println("");
         }
         System.out.println("");
-	}
+    }
 }
