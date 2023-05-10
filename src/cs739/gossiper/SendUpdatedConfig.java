436bbba38cf80be12bfe7e141f97f420f3637b75
spencrr <23708360+spencrr@users.noreply.github.com>
2023-05-10 14:10:51 -0500

    Update SendUpdatedConfig.java
    Send update to all

--- src/cs739/gossiper/SendUpdatedConfig.java
+++ src/cs739/gossiper/SendUpdatedConfig.java
@@ -1,23 +1,48 @@
 package cs739.gossiper;
 
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import cs739.gossiper.messages.Gossip;
 import cs739.gossiper.messages.UpdateConfigRequest;
 
 public class SendUpdatedConfig {
+    private static final Logger logger = LogManager.getLogger(SendUpdatedConfig.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Config config = new Config();
         config.backlog = 100;
+        UpdateConfigRequest updateRequest = new UpdateConfigRequest(config);
 
-        String host = args[0];
+        Gossip gossipRequest = new Gossip(new ArrayList<Application>());
 
-        try (Socket socket = new Socket(host, Config.get().bootstrapPort)) {
-            MessageHelper.send(socket.getOutputStream(), new UpdateConfigRequest(config));
+        try (Socket socket = new Socket(Config.get().bootstrapHost, Config.get().bootstrapPort)) {
+            logger.info("socket created -- sending message");
+            MessageHelper.send(socket.getOutputStream(), gossipRequest);
+            Gossip reply = (Gossip) MessageHelper.readMessage(socket.getInputStream());
+            System.out.println(reply);
             socket.close();
+
         } catch (Throwable t) {
-            System.err.println("failed to send UpdateConfigRequest to " + host + " " + t);
+            logger.error("failed to get gossip list from:" + Config.get().bootstrapHost, t);
         }
+
+        List<Thread> threads = new ArrayList<>();
+
+        for (Application application : gossipRequest.applications) {
+            Thread thread = new Thread(new SendMessage(application.address, updateRequest));
+            thread.start();
+            threads.add(thread);
+        }
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
     }
 
 }
